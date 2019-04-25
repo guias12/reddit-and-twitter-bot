16a163b0d2e1ad429352132bab8be21dade8a655
guias12 <guilherme.p.assis@gmail.com>
2019-04-24 21:43:32 -0300

    agora é possível fazer o download de e a postagem de diversas imagens

--- index.js
+++ index.js
@@ -25,33 +25,39 @@ getRedditData();
 
 function getRedditData(){
 	//using the reddit api to fetch data from r/blessedimages
-	redditConn.api.get("/r/blessedimages/new.json?sort=new", {limit: 5}).then(function(response) {
+	redditConn.api.get("/r/blessedimages/new.json?sort=hot", {limit: 5}).then(function(response) {
 	    var responseCode = response[0];
 	    var responseData = response[1];
-	    var imagesUrl = [];
+	    var count = 0;
 	    responseData.data.children.forEach(function(post){
-			imagesUrl.push(post.data.url);		
-		});
-		downloadImage(imagesUrl[1], 'testImage.jpg', function(){
-			tweetImage();
-		});
+	    	var imageName = 'image' + count + '.jpg';
+			downloadImage(post.data.url, imageName, function(){	
+				tweetImage(imageName, count);			
+			});
+			count++;
+		});		
 	}).catch(function(err) {
 	    return console.error("API request failed: " + err);
 	});
 };
 
 //downloading the image on the root directory(must improve :) )
-function downloadImage(uri, filename, callback){
+function downloadImage(uri, imageName, callback){
 	request.head(uri, function(err, res, body){
 	    console.log('content-type:', res.headers['content-type']);
 	    console.log('content-length:', res.headers['content-length']);
-	    request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
+	    console.log(imageName);
+	    request(uri).pipe(fs.createWriteStream(imageName)).on('close', callback);
   	});
 };
 
+
+
 //tweeting the downloaded image
-function tweetImage(){
-	var b64content = fs.readFileSync('./testImage.jpg', { encoding: 'base64' })
+function tweetImage(imageName, count){
+	var imagePath = './' + imageName;
+	var textStatus = 'blessed image ' + count;
+	var b64content = fs.readFileSync(imagePath, { encoding: 'base64' })
 	// Uploading media to twitter
 	T.post('media/upload', { media_data: b64content }, function (err, data, response) {	  
 		var mediaIdStr = data.media_id_string;
@@ -62,7 +68,7 @@ function tweetImage(){
 	    	if (!err) {
 	      		// now we can reference the media and post a tweet (media will attach to the tweet)
 	      		var params = {
-	      						status: 'Se voce esta lendo essa mensagem, e existe uma imagem aleatoria nesse tweet então eu sou o mago do node.js, caso contrario eu sou um fracasso. :)',
+	      						status: textStatus,
 	      						media_ids: [mediaIdStr]
 	      					};
 	      		T.post('statuses/update', params, function (err, data, response) {
