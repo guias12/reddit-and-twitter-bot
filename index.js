db34c33cf1221846b3503b4bb90f6e6b086bf21e
Arthur Teixeira <tutumgt@gmail.com>
2019-05-13 20:58:41 -0300

    Code refactor

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const redditConn = new RedditAPI({
     password:   process.env.RE_PASSWORD,
     app_id:     process.env.RE_APP_ID,
     api_secret: process.env.RE_API_SECRET,
-    user_agent: "Reddit-Watcher-V2",
+    user_agent: 'Reddit-Watcher-V2',
     logs: true
 });
 
@@ -26,8 +26,9 @@ getRedditData();
 
 function removeExistingImages(){
     try{
-        for(var a = 0; a < 2 ; a++){
-            var filePath = './img/image' + a + '.jpg'; 
+		let filePath;
+        for(let iterator = 0; iterator < 2 ; iterator++){
+            filePath = `./img/image'${iterator}.jpg`; 
             fs.unlinkSync(filePath);
         }
     } catch(e){
@@ -38,25 +39,20 @@ function removeExistingImages(){
 
 function getRedditData(){
 	//using the reddit api to fetch data from r/blessedimages
-	redditConn.api.get("/r/blessedimages/new.json?sort=new", {limit: 1}).then(function(response) {
-	    var responseCode = response[0];
-	    var responseData = response[1];
-	    var count = 0;
-	    responseData.data.children.forEach(function(post){
-	    	var imageName = './img/' + 'image' + count + '.jpg';
-			downloadImage(post.data.url, imageName, function(){	
-				tweetImage(imageName, count);			
-			});
+	redditConn.api.get('/r/blessedimages/new.json?sort=new', { limit: 1 }).then((response) => {
+	    const responseData = response[1];
+	    let count = 0;
+	    responseData.data.children.forEach((post) => {
+	    	const imageName = `./img/image${count}.jpg`;
+			downloadImage(post.data.url, imageName, tweetImage(imageName, count));
 			count++;
 		});		
-	}).catch(function(err) {
-	    return console.error("API request failed: " + err);
-	});
+	}).catch((err) => console.error(`API request failed: ${err}`));
 };
 
 //downloading the image on the root directory(must improve :) )
 function downloadImage(uri, imageName, callback){
-	request.head(uri, function(err, res, body){
+	request.head(uri, (err, res, body) => {
 	    console.log('content-type:', res.headers['content-type']);
 	    console.log('content-length:', res.headers['content-length']);
 	    console.log(imageName);
@@ -64,25 +60,20 @@ function downloadImage(uri, imageName, callback){
   	});
 };
 
-
-
 //tweeting the downloaded image
 function tweetImage(imageName, count){
-    var randomImageCode = Math.floor(1000 + Math.random() * 9000);
-	var textStatus = 'blessed image ' + randomImageCode;
-	var b64content = fs.readFileSync(imageName, { encoding: 'base64' })
+    const randomImageCode = Math.floor(1000 + Math.random() * 9000);
+	const textStatus = `blessed image${randomImageCode}`;
+	const b64content = fs.readFileSync(imageName, { encoding: 'base64' })
 	// Uploading media to twitter
-	T.post('media/upload', { media_data: b64content }, function (err, data, response) {	  
-		var mediaIdStr = data.media_id_string;
-	  	var meta_params = { media_id: mediaIdStr}
-		T.post('media/metadata/create', meta_params, function (err, data, response) {
+	T.post('media/upload', { media_data: b64content }, (err, data, response) => {	  
+		const mediaIdStr = data.media_id_string;
+	  	const meta_params = { media_id: mediaIdStr}
+		T.post('media/metadata/create', meta_params, (err, data, response) => {
 	    	if (!err) {
 	      		// now we can reference the media and post a tweet (media will attach to the tweet)
-	      		var params = {
-	      						status: textStatus,
-	      						media_ids: [mediaIdStr]
-	      					};
-	      		T.post('statuses/update', params, function (err, data, response) {
+	      		const params = { status: textStatus, media_ids: [mediaIdStr] };
+	      		T.post('statuses/update', params, (err, data, response) => {
 	        		console.log(data)
 	      		})
 	    	}
